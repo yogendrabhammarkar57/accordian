e73ffafba75be4a41c1b11389e542903a6793303
Yogendra <yogendrabhammarkar57@gmail.com>
2025-01-21 17:11:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const accordian1 = document.getElementById("acc1");
+const accordian2 = document.getElementById("acc2");
+const accordian3 = document.getElementById("acc3");
+const accordian4 = document.getElementById("acc4");
+const accordian5 = document.getElementById("acc5");
+
+
+function remove1(){
+    accordian1.classList.toggle("hidden");
+}
+function remove2(){
+    accordian2.classList.toggle("hidden");
+}
+function remove3(){
+    accordian3.classList.toggle("hidden");
+}
+function remove4(){
+    accordian4.classList.toggle("hidden");
+}
+function remove5(){
+    accordian5.classList.toggle("hidden");
+}
